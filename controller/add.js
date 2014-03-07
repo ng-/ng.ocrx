37c838eabdcc16d1375935a457bf52f24f0b48c4
Adam Kircher <adam.kircher@gmail.com>
2014-03-07 00:14:39 -0800

    Resize Complete!

--- controller/add.js
+++ controller/add.js
@@ -46,58 +46,9 @@ module.exports = function($scope, $window, $document, $location, fg, data)
 
 			context.rotate(Math.PI / 2)
 
-			context.drawImage(img, 0, 0, 3264/2, 2448/2, 0, 0, 800, 600)
-
-			//drawImageIOSFix(context, img, 0, 0, 3264, 2448, 0, 0, 800, 600)
+			context.drawImage(img, 0/2, 0/2, 3264/2, 2448/2, 0, 0, 800, 600)
 
 			fg.saveFile(folder.uuid, 'item'+$scope.count+'.jpg', canvas.toDataURL('image/jpeg'))
-		}
-
-		/**
-		 * Detecting vertical squash in loaded image.
-		 * Fixes a bug which squash image vertically while drawing into canvas for some images.
-		 * This is a bug in iOS6 devices. This function from https://github.com/stomita/ios-imagefile-megapixel
-		 *
-		 */
-		function detectVerticalSquash(img) {
-			 var iw = img.naturalWidth, ih = img.naturalHeight;
-			 var canvas = document.createElement('canvas');
-			 canvas.width = 1;
-			 canvas.height = ih;
-			 var ctx = canvas.getContext('2d');
-			 ctx.drawImage(img, 0, 0);
-			 var data = ctx.getImageData(0, 0, 1, ih).data;
-			 // search image edge pixel position in case it is squashed vertically.
-			 var sy = 0;
-			 var ey = ih;
-			 var py = ih;
-			 while (py > sy) {
-				  var alpha = data[(py - 1) * 4 + 3];
-				  if (alpha === 0) {
-						ey = py;
-				  } else {
-						sy = py;
-				  }
-				  py = (ey + sy) >> 1;
-			 }
-			 var ratio = (py / ih);
-			 return (ratio===0)?1:ratio;
-		}
-
-		/**
-		 * A replacement for context.drawImage
-		 * (args are for source and destination).
-		 */
-		function drawImageIOSFix(ctx, img, sx, sy, sw, sh, dx, dy, dw, dh) {
-			 var vertSquashRatio = detectVerticalSquash(img);
-
-			 $scope.vertical = '    '+vertSquashRatio+'   '+img.width+'  '+img.height
-		 // Works only if whole image is displayed:
-		 // ctx.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh / vertSquashRatio);
-		 // The following works correct also when only a part of the image is displayed:
-			 ctx.drawImage(img, sx * vertSquashRatio, sy * vertSquashRatio,
-									  sw * vertSquashRatio, sh * vertSquashRatio,
-									  dx, dy, dw, dh );
 		}
 
 		img.src = URL.createObjectURL(files[0])
