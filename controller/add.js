f3cbe7e761d714b048ae92c820bbbe08611cd254
Adam Kircher <adam.kircher@gmail.com>
2014-03-06 21:36:13 -0800

    Tried manually sizing iPhone squash

--- controller/add.js
+++ controller/add.js
@@ -46,7 +46,7 @@ module.exports = function($scope, $window, $document, $location, fg, data)
 
 			context.rotate(Math.PI / 2)
 
-			context.drawImage(img, 0, 0, canvas.width, canvas.height)
+			context.drawImage(img, 0, 0, canvas.height, canvas.height*1.333333)
 
 			fg.saveFile(folder.uuid, 'item'+$scope.count+'.jpg', canvas.toDataURL('image/jpeg'))
 		}
