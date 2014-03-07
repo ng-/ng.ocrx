45433dded2ce3fde7dd33752b215669942e6901c
Adam Kircher <adam.kircher@gmail.com>
2014-03-07 00:03:51 -0800

    Resize Fix

--- controller/add.js
+++ controller/add.js
@@ -46,7 +46,7 @@ module.exports = function($scope, $window, $document, $location, fg, data)
 
 			context.rotate(Math.PI / 2)
 
-			context.drawImage(img, 0, 0, 3264, 2448, 0, 0, 800, 1200)
+			context.drawImage(img, 0, 0, 3264, 2448, 0, 0, 1600, 1200)
 
 			//drawImageIOSFix(context, img, 0, 0, 3264, 2448, 0, 0, 800, 600)
 
