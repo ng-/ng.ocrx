64b4a2423309e57d8570fd06cd03152d236b9a99
Adam Kircher <adam.kircher@gmail.com>
2014-03-07 10:05:13 -0800

    Add comments to explain resize and try to fix safari formatting

--- controller/add.js
+++ controller/add.js
@@ -36,8 +36,8 @@ module.exports = function($scope, $window, $document, $location, fg, data)
 
 			var canvas = document.createElement('canvas')
 
-			canvas.width  = 600 //Default w/rotation iPhone: 2448px
-			canvas.height = 800 //Default w/rotation iPhone: 3264px
+			canvas.width  = 600
+			canvas.height = 800
 			// context.translate(canvas.width/2,canvas.height/2);
 
 			var context = canvas.getContext('2d')
@@ -46,7 +46,9 @@ module.exports = function($scope, $window, $document, $location, fg, data)
 
 			context.rotate(Math.PI / 2)
 
-			context.drawImage(img, 0/2, 0/2, 3264/2, 2448/2, 0, 0, 800, 600)
+			//Default iPhone: 3264px x 2448px w/ vertical squash of .5 as calculated by
+			//http://stackoverflow.com/questions/11929099/html5-canvas-drawimage-ratio-bug-ios
+			context.drawImage(img, 0/2, 0/2, 3264/2, 2448/2, 0, 0, canvas.height, canvas.width)
 
 			fg.saveFile(folder.uuid, 'item'+$scope.count+'.jpg', canvas.toDataURL('image/jpeg'))
 		}
