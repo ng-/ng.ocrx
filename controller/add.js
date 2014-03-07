f3df998cec76733c9fad966debf3641ec62a37b7
Adam Kircher <adam.kircher@gmail.com>
2014-03-06 23:05:58 -0800

    Keep Trying

--- controller/add.js
+++ controller/add.js
@@ -46,7 +46,7 @@ module.exports = function($scope, $window, $document, $location, fg, data)
 
 			context.rotate(Math.PI / 2)
 
-			context.drawImage(img, 0, 0, 1200, 900)
+			context.drawImage(img, 0, 0, 1400, 1066)
 
 			fg.saveFile(folder.uuid, 'item'+$scope.count+'.jpg', canvas.toDataURL('image/jpeg'))
 		}
