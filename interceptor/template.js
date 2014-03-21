2d9d9b18bd84b67aab1c3580679b5de89ac85d87
Adam Kircher <adam.kircher@gmail.com>
2014-03-20 21:10:26 -0700

    Logo is now Facetime Link

--- interceptor/template.js
+++ interceptor/template.js
@@ -10,6 +10,7 @@ exports.server = function()
 			[
 				"<html ng-app='ng.ocrx'>",
 					'<head>',
+						'<title>OCRx</title>',
 						'<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">',
 						'<meta name="apple-mobile-web-app-capable" content="yes">',
 						"<link href='//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css' rel='stylesheet'>",
@@ -19,7 +20,7 @@ exports.server = function()
 						ng,
 					"</head>",
 					"<body>",
-						"<div class='pull-right' style='margin-right:15px'><span class='text-title'>OC<span class='text-danger'>R</span></span><span class='text-danger'>X</span></div>",
+						"<a href='facetime:16504887434'><div class='pull-right' style='margin-right:15px'><span class='text-title'>OC<span class='text-danger'>R</span></span><span class='text-danger'>X</span></div></a>",
 						"<div class='ng-view'></div>",
 					"</body>",
 				"</html>"
