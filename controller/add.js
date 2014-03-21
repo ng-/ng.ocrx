9656e5decefefba60f4b3abca56af5f8b21ad3fa
Adam Kircher <adam.kircher@gmail.com>
2014-03-20 18:17:38 -0700

    Testing Centering

--- controller/add.js
+++ controller/add.js
@@ -19,7 +19,9 @@ module.exports = function($scope, $window, $document, $location, fg, data)
 
 	var rand = Math.random()
 
-	$scope.count = rand < .333 ? (rand < .666 ? Math.floor(Math.random()*900) + 100 : Math.floor(Math.random()*10)) : Math.floor(Math.random()*90) + 10//data('count')
+	console.log(rand)
+
+	$scope.count = rand > .333 ? (rand < .666 ? Math.floor(Math.random()*900) + 100 : Math.floor(Math.random()*10)) : Math.floor(Math.random()*90) + 10//data('count')
 
 	$scope.done = function()
 	{
