e309b0c852bd7d7fb074842625c172d5ab37f1f2
Adam Kircher <adam.kircher@gmail.com>
2014-03-23 16:07:38 -0700

    Improve performance by removing redundant operations

--- controller/add.js
+++ controller/add.js
@@ -2,10 +2,15 @@
 
 module.exports = function($scope, $window, $document, $location, fg, data)
 {
-	'use strict'
-
-	var user   = data('user')
-	  , folder = data('folder')
+	var user    = data('user')
+	  , folder  = data('folder')
+	  , canvas  = document.createElement('canvas')
+	  , context = canvas.getContext('2d')
+	  //Default iPhone: 3264px x 2448px w/ vertical squash of .5 as calculated by
+	  //http://stackoverflow.com/questions/11929099/html5-canvas-drawimage-ratio-bug-ios
+	  , squish  = 'iPhone' == navigator.platform ? 2 : 1
+	  , scale   = 1
+	  , img     = new Image
 
 	if ( ! user)
 	{
@@ -28,22 +33,25 @@ module.exports = function($scope, $window, $document, $location, fg, data)
 	{
 		$scope.count = data('count+', 1)
 
-		var img = new Image
-
 		img.onload = function()
 		{
 			URL.revokeObjectURL(img.src)
 
-			var canvas = document.createElement('canvas')
+			canvas.width  = img.width  * scale
+			canvas.height = img.height * scale
 
-			canvas.width  = img.width  * 1
-			canvas.height = img.height * 1
-
-			var context = canvas.getContext('2d')
-
-			//Default iPhone: 3264px x 2448px w/ vertical squash of .5 as calculated by
-			//http://stackoverflow.com/questions/11929099/html5-canvas-drawimage-ratio-bug-ios
-			context.drawImage(img, 0/2, 0/2, img.width/2, img.height/2, 0, 0, canvas.width, canvas.height)
+			context.drawImage
+			(
+				img,
+				0/squish,
+				0/squish,
+				img.width/squish,
+				img.height/squish,
+				0,
+				0,
+				canvas.width,
+				canvas.height
+			)
 
 			//Pad the digits to help with alphabetical ordering in foldergrid
 			var pad = $scope.count < 10 ? '00' : $scope.count < 100 ? '0' : ''
